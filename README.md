8c5899f01691abd7a8ca83f4bbaf4fe60879f075
Jiri Spitalsky (DCIM ext) <jura@spital.cz>
2021-05-09 20:40:20 +0200

    Added async, tags, test, url params, Dockerfile, readme, fixed delete, TODO auth, router, frontend GUI

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# fastapi-test-blog
+# Simple blog management Rest API in Python using Fast API
+
+Example to get acquainted with the framework, no frontend and no real database, just storing data in the memory.
+
+Main functions:
+
+* Creating a new blog post
+* Editing blog post
+* Deleting blog post
+* List blog post
+* View blog post detail
+
+
+## Requirements
+
+
+`sudo dnf install podman` or [Install Docker Engine](https://docs.docker.com/install/).
+
+`sudo dnf install podman-compose` or [Install Docker Compose](https://github.com/docker/compose/releases/latest).
+
+`sudo dnf install git` or [Install git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git).
+
+
+## Build
+Please note this is example development build.
+
+```bash
+git clone https://github.com/spital/fastapi-test-blog
+cd fastapi-test-blog
+podman-compose build
+```
+
+## Source tests
+*TODO*: pylint, unittest, sonarqube, ...
+
+
+## Run
+
+```bash
+podman-compose up -d
+podman-compose logs -f web
+```
+
+## Api test
+
+```bash
+podman exec -it fastapi-test-blog_web_1 sh -c 'pytest /usr/src/app'
+```
+
+## Use
+Start web browser and go to (http://localhost:8002/) for API root
+
+or (http://localhost:8002/redoc) for API docs
+
+or (http://localhost:8002/docs) for API docs with tryouts.
+
--- app.py
+++ app.py
@@ -1,21 +1,47 @@
 # coding=utf-8
 
 # inspired by https://dev.to/xarala221/python-rest-apis-with-fastapi-crud-application-9kc
+#   ... FastAPI\ -\ A\ python\ framework\ _\ Full\ Course-7t2alSnE2-I
+#   ... https://testdriven.io/blog/fastapi-crud/
+#   ... https://fastapi.tiangolo.com/async/
+#   ... https://github.com/pluralsight/tech-blog-fastapi-demo
+#   ... https://github.com/mjhea0/awesome-fastapi
+#   ... https://www.toptal.com/python/build-high-performing-apps-with-the-python-fastapi-framework
+#   ... https://phrase.com/blog/posts/fastapi-i18n/
 
 # app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Optional, Text
 from datetime import datetime
 
 
 app = FastAPI()
-postdb = []
+postdb = [
+{
+  "idno": 11,
+  "title": "Getting started with FastAPI",
+  "author": "George",
+  "content": "Hello FastAPI",
+  "created_at": "2021-05-05T10:10:10.1010Z",
+  "published_at": "2021-05-05T11:11:11.1111Z",
+  "published": True
+},
+{
+  "idno": 22,
+  "title": "Getting started with FastAPI",
+  "author": "George",
+  "content": "Added async, tags, test, url params, Dockerfile, readme, fixed delete, TODO auth, router, frontend GUI",
+  "created_at": "2021-05-09T10:10:10.1010Z",
+  "published_at": "2021-05-09T11:11:11.1111Z",
+  "published": True
+}
+]
 
 
 # post model
 class Post(BaseModel):
-    id: int
+    idno: int
     title: str
     author: str
     content: Text
@@ -24,40 +50,50 @@ class Post(BaseModel):
     published: Optional[bool] = False
 
 
-@app.get("/")
-def read_root():
-  return {"home": "easy blog management Rest API in Python using Fast API"}
+@app.get("/", tags=['Test'])
+async def read_root():
+    """just return something for the root path"""
+    return {"home": "easy blog management Rest API in Python using Fast API"}
 
 
-@app.get("/blog")
-def list_blog_posts():
-    """List blog posts"""
-    return postdb
+@app.get("/ping", tags=['Test'])
+async def pong():
+    return {"ping": "pong!"}
 
 
-@app.post("/blog")
-def create_new_post(post: Post):
+@app.get("/blog", tags=['Blog'])
+async def list_blog_posts(limit:int=10, startfrom:int=0):
+    """List blog posts: ?limit=10&startfrom=0"""
+    return postdb[startfrom:startfrom+limit]
+
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=['Auth TODO'])
+async def create_new_post(post: Post):
     """Creating a new blog post - CREATE"""
     postdb.append(post.dict())
     return postdb[-1]
 
 
-@app.get("/blog/{post_id}")
-def view_post_detail(post_id: int):
-    """View blog post detail - READ"""
-    post = post_id - 1
-    return postdb[post]
+@app.get("/blog/{post_id}", status_code = status.HTTP_200_OK, tags=['Blog'])
+async def view_post_detail(post_id: int, response: Response):
+    """View blog post detail - READ - zero based index in blog list"""
+    if post_id >= len(postdb):
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": f"error: post_id {post_id} out of range, postdb length is {len(postdb)}"}
+    else:
+        return postdb[post_id]
 
 
-@app.post("/blog/{post_id}")
-def edit_post(post_id: int, post: Post):
-    """Editing blog post - UPDATE"""
+@app.put("/blog/{post_id}", status_code = status.HTTP_202_ACCEPTED, tags=['Auth TODO'])
+async def edit_post(post_id: int, post: Post):
+    """Editing blog post - UPDATE - zero based index in blog list"""
     postdb[post_id] = post
     return {"message": "Post has been updated succesfully"}
 
 
-@app.delete("/blog/{post_id}")
-def delete_post(post_id: int):
-    """Deleting blog post - DELETE"""
-    postdb.pop(post_id-1)
+@app.delete("/blog/{post_id}", status_code = status.HTTP_204_NO_CONTENT, tags=['Auth TODO'])
+async def delete_post(post_id: int):
+    """Deleting blog post - DELETE - zero based index in blog list"""
+    postdb.pop(post_id)
     return {"message": "Post has been deleted succesfully"}
+
